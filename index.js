bb758ecd56649683875bc36232db3b32af6664ee
cgortonbrooks <cgorton@brooksschool.org>
2025-04-21 14:28:17 -0400

    first class

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+    host: '66.198.240.46',
+    user: 'bfzhiwes_node_intro_user',
+    password: 'node-intro-user-password',
+    database: 'bfzhiwes_node_intro'
+})
+
+app.get('/students', (req, res) => {
+    let sql = 'select * from students'
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).send(err)
+        res.json(results)
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hi</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
