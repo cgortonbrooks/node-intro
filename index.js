0d72c96dfecdb7b66b54c3b05adb7994edcf4bcb
cgortonbrooks <cgorton@brooksschool.org>
2025-04-21 14:41:39 -0400

    fixed underscores

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const PORT = 3000
 const mysql = require('mysql2')
 const db = mysql.createConnection({
     host: '66.198.240.46',
-    user: 'bfzhiwes_node_intro_user',
+    user: 'bfzhiwes_node-intro-user',
     password: 'node-intro-user-password',
-    database: 'bfzhiwes_node_intro'
+    database: 'bfzhiwes_node-intro',
+    port: 3306
 })
 
 app.get('/students', (req, res) => {
     let sql = 'select * from students'
     db.query(sql, (err, result) => {
         if (err) return res.status(500).send(err)
-        res.json(results)
+        res.json(result)
     })
 })
 
