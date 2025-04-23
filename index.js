59d1bd7481786e99b08d935a967274e8c26c9afa
cgortonbrooks <cgorton@brooksschool.org>
2025-04-23 09:06:55 -0400

    dat 2 progress

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.static('public'))
+
 const mysql = require('mysql2')
 const db = mysql.createConnection({
     host: '66.198.240.46',
     user: 'bfzhiwes_node-intro-user',
     password: 'node-intro-user-password',
-    database: 'bfzhiwes_node-intro',
-    port: 3306
+    database: 'bfzhiwes_node-intro'
 })
 
 app.get('/students', (req, res) => {
@@ -17,10 +18,15 @@ app.get('/students', (req, res) => {
         if (err) return res.status(500).send(err)
         res.json(result)
     })
+    console.log("someone hit the DB")
 })
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hi</h1>')
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+app.get('/form', (req, res) => {
+    res.sendFile(`${__dirname}/public/form.html`)
 })
 
 app.listen(PORT, () => {
