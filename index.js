d01d2b51499c0babe8cd117bf05e71534e7de535
cgortonbrooks <cgorton@brooksschool.org>
2025-04-24 15:04:44 -0400

    got insert working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 const mysql = require('mysql2')
 const db = mysql.createConnection({
@@ -27,6 +28,19 @@ app.get('/', (req, res) => {
 
 app.get('/form', (req, res) => {
     res.sendFile(`${__dirname}/public/form.html`)
+})
+
+app.post('/add-student', (req, res) => {
+    let name = req.body.name
+    let grade = req.body.grade
+    console.log(`Hey ${name} you got a ${grade}`)
+    let sql = `insert into students (name, grade)
+        values ("${name}", ${grade})
+    `
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).send(err)
+        res.redirect('/students')
+    })
 })
 
 app.listen(PORT, () => {
